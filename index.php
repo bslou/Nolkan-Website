8ba2ee2a3d2f9090f96b3e9cbecbe0fcaee9dec0
bslou <64224906+bslou@users.noreply.github.com>
2020-07-08 23:15:50 -0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,103 @@
+<html>
+  <head>
+    <title>Nolkan Website</title>
+    <link rel = "icon" href = "logo.png">
+    <style>
+    a{
+      text-decoration: none;
+      color: black;
+    }
+    a:hover{
+      text-decoration: underline;
+    }
+    a:focus{
+      color: orange;
+    }
+    .klkl{
+      width: 300px;
+      height: 300px;
+    }
+    .none:hover{
+      text-decoration:none;
+    }
+    tr, td, th{
+      padding-right: 40px;
+      padding-bottom: 10px;
+    }
+    td a{
+      color: gray;
+    }
+    .sticky {
+      position: fixed;
+      top: 0;
+      width: 100%
+    }
+    .but:hover{
+      opacity: 0.8;
+    }
+    </style>
+  </head>
+  <body>
+  <center>
+    <div class = "container" id = "myHeader" style = "background-color: white;">
+    <a href = "index.php"><img src = "logo.png" style = "width: 50px; height: 20px;"></a>
+      <a href = "index.php" style = "padding: 10px; font-size: 17pt; font-family: serif;">Home</a><a href = "#" style = "padding: 10px; font-size: 17pt; font-family: serif;">About</a><a href = "#" style = "padding: 10px; font-size: 17pt; font-family: serif;">Goal</a><a href = "#" style = "padding: 10px; font-size: 17pt; font-family: serif;">Downloads</a><a href = "#" style = "padding: 10px; font-size: 17pt; font-family: serif;">Contact Us</a>
+    </div>
+    </center>
+    <div style = "background-color: black;">
+    <br><br>
+    <center><h1 style = "color: white; font-family: serif; letter-spacing: 2.3px; font-size: 30pt;">Make Funny Acting Videos</h1></center>
+
+    <br><br>
+    <center>
+    <a href = "#" class = "none"><img src = "https://freesvg.org/img/Punch-by-Rones.png" class = "klkl"></a>
+    <a href = "#" class = "none">
+    <img src = "https://www.clipartmax.com/png/full/244-2445582_moustache-villain-clip-art-evil-twirling-mustache.png" class = "klkl"></a>
+    <a href = "#" class = "none">
+    <img src = "https://cdn.pixabay.com/photo/2019/10/29/16/41/cartoons-4587462_1280.png" class = "klkl">
+    </center></a>
+    <br><br><br>
+    <center><a href = "#" style = "color: black; background-color: white; font-size: 25pt; padding: 10px; border-radius: 25px 25px 25px 25px; font-weight: 600;" class = "none but">Watch Now</a></center><br><br><br><div><center><a href = "#"><img src = "https://lh3.googleusercontent.com/qF9r3ZjtgG-qyHdmjecArtKiulz1gmwL_xl9R3_fzk6igSeoN0wYbJSKEX5d_fxJRwYZJpHbqcLB3i9atl-9dOfUl9an7U43TfZ9PtQ=s0"></a><a href = "#"><img src = "appstore.png" style = "width: 150px; height: 45px;"></a>
+    
+    </center>
+    <br>
+    </div>
+    </div>
+    <br><br>
+    <div>
+    <center>
+    <table>
+    <tr><th>Company</th><th>Community</th><th>Advertising</th><th>Legal</th></tr>
+    <tr><td><a href = "#">Nolkan Inc.</a></td><td><a href = "#">Support</a></td><td><a href = "#">Buy Ads</a></td><td><a href = "#">Cookie Policy</a></td></tr>
+<tr><td><a href = "#">Careers</a></td><td><a href = "#">Community Guidelines</a></td><td><a href = "#">Advertising Policy</a></td><td><a href = "#">Privacy Center</a></td></tr>
+<tr><td><a href = "#">News</a></td><td><a href = "#">Safety Center</a></td><td><a href = "#">Brand Guidelines</a></td><td><a href = "#">Terms of Service</a></td></tr>
+<tr><td><a href = "#"></a></td><td></td><td><a href = "#">Promotion Rules</a></td><td></td></tr>    
+    </table>
+    </center>
+    </div>
+    <br>
+    <div style = "background: linear-gradient(to right, red, orange, yellow, green, blue, purple); height: 5%;">
+    <hr>
+    <div>
+    <center>
+<a href = "#" style = "padding: 20px; color: white;">Privacy Policy</a><a href = "#" style = "padding: 20px; color: white;">Terms of Service</a>
+</center>
+    </div>
+    </div>
+    <center><p style = "opacity: 0.4;">© Nolkan Inc. 2020</p></center>
+    <script>
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("myHeader");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+</script>
+  </body>
+</html>
